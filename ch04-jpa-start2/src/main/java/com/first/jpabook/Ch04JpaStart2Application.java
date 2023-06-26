41019f52eea205f0bcb3f75932fca39695cc1518
ricky <kimzzan1234@gmail.com>
2023-06-26 22:52:00 +0900

    jpa 2.6 애플리케이션개발

---
+++ ch04-jpa-start2/src/main/java/com/first/jpabook/Ch04JpaStart2Application.java
@@ -0,0 +1,13 @@
+package com.first.jpabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch04JpaStart2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch04JpaStart2Application.class, args);
+	}
+
+}
---
+++ ch04-jpa-start2/src/test/java/com/first/jpabook/Ch04JpaStart2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.jpabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch04JpaStart2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
