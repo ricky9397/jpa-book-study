7934add87f180869db09d29e6854e094b36aa7b6
ricky <kimzzan1234@gmail.com>
2023-07-02 00:16:53 +0900

    5장

---
+++ ch05-model2/src/main/java/com/first/ch05model2/Ch05Model2Application.java
@@ -0,0 +1,13 @@
+package com.first.ch05model2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch05Model2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch05Model2Application.class, args);
+	}
+
+}
---
+++ ch05-model2/src/main/java/com/first/ch05model2/model/entity/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.first.ch05model2.model.entity;
+
+/**
+ * Created by holyeye on 2014. 3. 11..
+ */
+public enum OrderStatus {
+    ORDER, CANCEL
+}
